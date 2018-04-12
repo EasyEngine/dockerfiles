6f9007de8a685e92fd48e943672759fff0dc6702
Dharmin K Shah <castiel.shah@gmail.com>
2018-04-12 21:58:33 +0530

    Add root support

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -4,11 +4,15 @@ U_ID=${USER_ID}
 G_ID=${GROUP_ID}
 D_ID=${DOCKER_ID}
 
-groupadd -g $D_ID docker > /dev/null 2>&1
-useradd -u $U_ID ee > /dev/null 2>&1
-groupmod -g $G_ID ee > /dev/null 2>&1
-usermod -d $EE_HOME -u $U_ID ee > /dev/null 2>&1
-addgroup ee docker > /dev/null 2>&1
-addgroup ee sudo > /dev/null 2>&1
-echo "ee    ALL = NOPASSWD: ALL" >> /etc/sudoers
-gosu ee /usr/local/bin/ee4 $@
+if [[ "$U_ID" -ne 0 ]];then
+    groupadd -g $D_ID docker > /dev/null 2>&1
+    useradd -u $U_ID ee > /dev/null 2>&1
+    groupmod -g $G_ID ee > /dev/null 2>&1
+    usermod -d $EE_HOME -u $U_ID ee > /dev/null 2>&1
+    addgroup ee docker > /dev/null 2>&1
+    addgroup ee sudo > /dev/null 2>&1
+    echo "ee    ALL = NOPASSWD: ALL" >> /etc/sudoers
+    gosu ee /usr/local/bin/ee4 $@
+else
+    /usr/local/bin/ee4 $@ --allow-root
+fi
