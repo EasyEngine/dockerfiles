df3a57abda3f0e53a1f0314465a3d174b5c9cfb8
Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>
2018-11-16 14:36:50 +0530

    Remove unnecessary user and group changes
    Signed-off-by: Riddhesh Sanghvi <riddheshsanghvi96@gmail.com>

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -euo pipefail
 
-U_ID=${USER_ID}
-G_ID=${GROUP_ID}
-
-wwwdata_uid=$(id -u www-data)
-if [[ "$U_ID" -ne 0 &&  "$wwwdata_uid" -ne $U_ID ]];then
-	usermod -u $U_ID www-data
-	groupmod -g $G_ID www-data
-	chown -R www-data:www-data /var/
-fi
-
 exec "$@"
